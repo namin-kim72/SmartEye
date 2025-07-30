db24b53c2e1ecf08fcead8bdedae7f3b32840f7f
jyk222000 <rlawnsdud127@naver.com>
2025-07-30 19:54:57 +0900

    Update camera.py

--- Meals_mode/camera.py
+++ Meals_mode/camera.py
@@ -1,7 +1,15 @@
 # 카메라 접속하는 코드
 
 def camera_connection():
-    pass
+    print("카메라에 접속을 시도합니다.")
+    is_connected = True
+    if is_connected:
+        print("카메라에 성공적으로 연결되었습니다.")
+        return True
+    else:
+        print("오류: 카메라에 연결할 수 없습니다.")
+        return False
 
 def read_frame():
-    pass
+    print("현재 프레임을 캡처합니다.")
+    return "frame_data"
