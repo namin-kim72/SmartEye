c229659f07ed21134f7ad1e4e1896bb06ce9dc29
jyk222000 <rlawnsdud127@naver.com>
2025-07-30 19:56:25 +0900

    Update main.py

--- Meals_mode/main.py
+++ Meals_mode/main.py
@@ -1,28 +1,37 @@
 import camera
+import detect_food
 import detect_hands
-import detect_text
-import read_text
-import point_text
+import point_food
+import read_food
 
 def main():
     # 카메라 접속
-    camera.camera_connection()
+    if not camera.camera_connection():
+        print("프로그램을 종료합니다.")
+        return
 
-    while True:
-        # 카메라 프레임 받아오기
-        camera.read_frame()
+    try:
+        while True:
+            # 카메라 프레임 받아오기
+            frame = camera.read_frame()
 
-        # 프레임 내의 손 인식
-        detect_hands.detect_hands()
+            # 프레임 내의 손 인식
+            hand_info = detect_hands.detect_hands(frame)
 
-        # 만약 손이 무언가를 가르키고 있다면
-        if detect_hands.hands_pointing() == True:
-            # 손이 가르키는 문장 인식
-            detect_text.detect_text()
-            point_text.find_text()
 
-            # 인식된 문장 읽어주기
-            read_text.read_text()
+            # 만약 손이 무언가를 가르키고 있다면
+            if detect_hands.hands_pointing(hand_info) == True:
+
+               # 손이 가르키는 음식 인식
+               food_data = detect_food.detect_food(frame)
+
+               food_to_read = point_food.find_food(hand_info["position"], food_data)
+
+               # 인식된 음식 읽어주기
+               read_food.read_food(food_to_read)
+
+    except KeyboardInterrupt:
+        print("\n\n프로그램을 종료합니다.")
 
 if __name__ == '__main__':
     main()
@@ -36,10 +45,10 @@ if __name__ == '__main__':
 
 딥러닝 모델로 프레임 내의 손 인식
 
-손이 문장을 가르키고 있다면
+손이 음식을 가르키고 있다면
 
-    딥러닝 모델로 손이 가르키는 문장 인식
-    TTS로 문장 읽어주기
+    딥러닝 모델로 손이 가르키는 음식을 인식
+    TTS로 어떤 음식인지 읽어주기
     
     
 자세한 파이프라인 없이 print() 구문으로 전체 흐름도를 완성
